f6a8714c37dcf01ea6494dd840dbdcd7ba82051e
Thiran Sasanka <136495048+thiran09@users.noreply.github.com>
2023-08-07 23:01:35 +0530

    Update Q3.scala

--- Labsheet 7/Q3.scala
+++ Labsheet 7/Q3.scala
@@ -1,20 +1,16 @@
 object Q3 {
-  def main(args: Array[String]): Unit = {
-    val inputList = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    val outputList = filterPrime(inputList)
-    println(outputList)
-  }
-
-  def filterPrime(inputList: List[Int]): List[Int] = {
-    def isPrime(num: Int): Boolean = {
-      if (num <= 1) false
-      else if (num <= 3) true
-      else {
-        val sqrtNum = math.sqrt(num).toInt
-        !(2 to sqrtNum).exists(num % _ == 0)
-      }
+def filterPrime(numbers: List[Int]): List[Int] = {
+    def isPrime(n: Int): Boolean = {
+      if (n <= 1) false
+      else if (n == 2) true
+      else !(2 to Math.sqrt(n).toInt).exists(x => n % x == 0)
     }
 
-    inputList.filter(isPrime)
+    numbers.filter(isPrime)
   }
-}
+
+  def main(args: Array[String]): Unit = {
+    val inputList = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val primeList = filterPrime(inputList)
+    println(primeList) // Output: List(2, 3, 5, 7)
+  }
